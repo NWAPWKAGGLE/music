8991c48aaebe9c93ee81e2b9c4475ca7542d567c
Eli Winkelman <eliwinkelman@Elis-MacBook-Pro-2.local>
2017-07-31 14:50:27 -0700

    works?

--- lstm.py
+++ lstm.py
@@ -25,41 +25,63 @@ def get_songs(path):
 
 #Hyperparams
 learning_rate = .05
-layer_units = 127
+epochs = 10
+layer_units = 156
 batch_size = 100
 n_steps = 100
 songs = get_songs('./beeth')
-notes_in_dataset = []
+notes_in_dataset = songs
+'''
 for i in range(len(songs)):
     notes_in_dataset.append(split_list(songs[i], batch_size))
+'''
 
 
-initial_state = state = tf.zeros([batch_size, lstm.state_size])
-probabilities = []
-loss = 0.0
-w = tf.Variable('softmax_w', tf.random_normal([layer_units, 127]))
-b = tf.Variable('softmax_b', tf.random_normal([127]))
-x = tf.placeholder([None, 100, 127])
-y = tf.placeholder([None, 127])
+w = tf.Variable(tf.truncated_normal([layer_units, 156], stddev=.1))
+b = tf.Variable(tf.truncated_normal([156], stddev=.1))
+x = tf.placeholder(tf.float32, (None, 100, 156))
+y = tf.placeholder(tf.float32, (None, 156))
 
 def RNN(x, w, b):
 
-    x = tf.unstack(x, n_steps, 1)
-
-    lstm_cell = tf.contrib.dynamic_rnn.BasicLSTMCell(layer_units)
+    lstm_cell = tf.contrib.rnn.BasicLSTMCell(layer_units)
 
     outputs, states = tf.nn.dynamic_rnn(lstm_cell, x, dtype=tf.float32)
 
     return tf.sigmoid(tf.matmul(outputs[-1], w) + b)
 
+print(x, songs)
 pred = RNN(x, w, b)
-
+print('yes')
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-correct_pred = tf.equal(tf.round(pred))
+correct_pred = tf.equal(tf.round(pred), y)
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
-    
+    sess.run(init)
+    step = 0
+
+    for i in tqdm(range(epochs)):
+        input_sequence = []
+        expected_output = []
+        for song in songs:
+            for offset in range(len(song)-n_steps-1):
+
+                input_sequence.append(song[offset:offset+n_steps])
+                expected_output.append(song[offset+n_steps+1])
+
+        print(input_sequence, expected_output)
+
+        sess.run(optimizer, feed_dict={x: input_sequence, y: expected_output})
+
+print(sess.run(cost,feed_dict={x: input_sequence, y: expected_output}))
+
+
+
+
+
+
+
