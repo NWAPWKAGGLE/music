23aa27dfdc3312357ed20a0ec262f55e4fac1536
Eli Winkelman <eliwinkelman@Elis-MacBook-Pro-2.local>
2017-07-31 11:19:11 -0700

    load and format data

--- lstm.py
+++ lstm.py
@@ -1,16 +1,53 @@
 import tensorflow as tf
-from midi import noteStateMatrixToMidi
+import glob
+from tqdm import tqdm
+import numpy as np
+from midi_manipulation import midiToNoteStateMatrix
 
+def split_list(l, n):
+    list = []
+    for j in range(0, len(l), n):
+        if (j+n < len(l)):
+            list.append(np.array(l[j:j+n]))
+    return list
 
+def get_songs(path):
+    files = glob.glob('{}/*.mid*'.format(path))
+    songs = []
+    for f in tqdm(files):
+        try:
+            song = np.array(midiToNoteStateMatrix(f))
+            if np.array(song).shape[0] > 50:
+                songs.append(song)
+        except Exception as e:
+            raise e
+    return songs
 
 #Hyperparams
 learning_rate = .05
 layer_units = 10
-batch_size = 20
+batch_size = 100
 lstm = tf.contrib.rnn.BasicLSTMCell(layer_units)
 state = tf.zeros([batch_size, layer_units])
 
-for current_batch_of_notes in notes_in_dataset
+songs = get_songs('./beeth')
+notes_in_dataset = []
+for i in range(len(songs)):
+    print(split_list(songs[i], batch_size))
+    notes_in_dataset.append(split_list(songs[i], batch_size))
+
+print(notes_in_dataset)
+
+
+'''
+for current_batch_of_notes in notes_in_dataset:
+    output, state = lstm(current_batch_of_notes, state)
+    
+    
+final_state = state    
+'''
+
+
 
 
 
