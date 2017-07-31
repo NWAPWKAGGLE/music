a3e5714ae386f526d41cb330da795137733ebbd9
Eli Winkelman <eliwinkelman@Elis-MacBook-Pro-2.local>
2017-07-31 13:34:11 -0700

    started building model

--- lstm.py
+++ lstm.py
@@ -25,14 +25,41 @@ def get_songs(path):
 
 #Hyperparams
 learning_rate = .05
-layer_units = 10
+layer_units = 127
 batch_size = 100
-
+n_steps = 100
 songs = get_songs('./beeth')
 notes_in_dataset = []
 for i in range(len(songs)):
-    print(split_list(songs[i], batch_size))
     notes_in_dataset.append(split_list(songs[i], batch_size))
 
 
+initial_state = state = tf.zeros([batch_size, lstm.state_size])
+probabilities = []
+loss = 0.0
+w = tf.Variable('softmax_w', tf.random_normal([layer_units, 127]))
+b = tf.Variable('softmax_b', tf.random_normal([127]))
+x = tf.placeholder([None, 100, 127])
+y = tf.placeholder([None, 127])
 
+def RNN(x, w, b):
+
+    x = tf.unstack(x, n_steps, 1)
+
+    lstm_cell = tf.contrib.dynamic_rnn.BasicLSTMCell(layer_units)
+
+    outputs, states = tf.nn.dynamic_rnn(lstm_cell, x, dtype=tf.float32)
+
+    return tf.sigmoid(tf.matmul(outputs[-1], w) + b)
+
+pred = RNN(x, w, b)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+correct_pred = tf.equal(tf.round(pred))
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    
