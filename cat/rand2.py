0e941b0cad5ac0cfd9ef89844068d686078909ae
Jensen Turner <jenst18@lakesideschool.org>
2017-08-01 11:27:29 -0700

    data generator?

---
+++ cat/rand2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+total_series_length = 10
+echo_step = 3
+batch_size = 5
+
+
+def generate_data():
+    x = np.array(np.random.choice(2, total_series_length, p=[0.5, 0.5]))
+    y = np.roll(x, echo_step)
+    y[0:echo_step] = 0
+    x = x.reshape((batch_size, -1))  # The first index changing slowest, subseries as rows
+    y = y.reshape((batch_size, -1))
+    return (x, y)
+
+generate_data()
+
+
