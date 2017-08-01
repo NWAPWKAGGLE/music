0e6f1ab493ec74a17e3c8f2c985c105b86186817
Eli Winkelman <eliwinkelman@Elis-MacBook-Pro-2.local>
2017-08-01 11:24:33 -0700

    Cleaning

--- lstm.py
+++ lstm.py
@@ -4,16 +4,11 @@ from tqdm import tqdm
 import numpy as np
 from midi_manipulation import midiToNoteStateMatrix
 
-
-def split_list(l, n):
-
-    list = []
-    for j in range(0, len(l), n):
-        if (j+n < len(l)):
-            list.append(np.array(l[j:j+n]))
-    return list
-
 def get_songs(path):
+    '''
+    :param path: path to the songs directory
+    :return: array of songs w/ timestamp events
+    '''
     files = glob.glob('{}/*.mid*'.format(path))
     songs = []
     for f in tqdm(files):
@@ -29,26 +24,40 @@ def get_songs(path):
 learning_rate = .05
 #Number of training
 epochs = 10
+#number of features
+num_features = 156
+#Layers in the hidden lstm layer
 layer_units = 156
+#Number of time steps to use
 n_steps = 10
+#Song directore
 songs = get_songs('./beeth')
-notes_in_dataset = songs
 
-w = tf.Variable(tf.truncated_normal([layer_units, 156], stddev=.1))
-b = tf.Variable(tf.truncated_normal([156], stddev=.1))
-x = tf.placeholder(tf.float32, (None, 10, 156))
-y = tf.placeholder(tf.float32, (None, 156))
+#Weights biases and placeholders
+w = tf.Variable(tf.truncated_normal([layer_units, num_features], stddev=.1))
+b = tf.Variable(tf.truncated_normal([num_features], stddev=.1))
 
-def RNN(x, w, b):
+#x is examples by time by features
+x = tf.placeholder(tf.float32, (None, 10, num_features))
+#y is examples by examples by features
+y = tf.placeholder(tf.float32, (None, num_features))
 
+def RNN(x):
+    '''
+    :param x: rnn input data
+    :return: rnn last timestamp output
+    '''
     lstm_cell = tf.contrib.rnn.BasicLSTMCell(layer_units)
 
-    outputs, states = tf.nn.dynamic_rnn(lstm_cell, x, dtype=tf.float32, time_major=True)
+    outputs, states = tf.nn.dynamic_rnn(lstm_cell, x, dtype=tf.float32)
+
     return tf.sigmoid(tf.matmul(tf.transpose(outputs, perm=[1, 0, 2])[-1], w) + b)
 
-pred = RNN(x, w, b)
+pred = RNN(x)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+# Cross entropy loss
+cost = tf.losses.softmax_cross_entropy(y, logits=pred)
+#Train with Adam Optimizer
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 correct_pred = tf.equal(tf.round(pred), y)
@@ -57,17 +66,14 @@ init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
-    step = 0
 
+    input_sequence = []
+    expected_output = []
+    for song in songs:
+        for offset in range(len(song) - n_steps - 1):
+            input_sequence.append(song[offset:offset + n_steps])
+            expected_output.append(song[offset + n_steps + 1])
     for i in tqdm(range(epochs)):
-        input_sequence = []
-        expected_output = []
-        for song in songs:
-            for offset in range(len(song)-n_steps-1):
-
-                input_sequence.append(song[offset:offset+n_steps])
-                expected_output.append(song[offset+n_steps+1])
-
         sess.run(optimizer, feed_dict={x: input_sequence, y: expected_output})
 
     print(sess.run(cost,feed_dict={x: input_sequence, y: expected_output}))
