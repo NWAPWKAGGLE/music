1fd42379362e2d517428031924cbb161108eb7d2
jensenturner <jenst18@lakesideschool.org>
2017-08-09 14:44:46 -0700

    add 20 epochs of lstm traning per main iteration

--- music.py
+++ music.py
@@ -45,6 +45,7 @@ lstm.start_sess(load_from_saved=load_from_saved)
 
 for j in range(100):
     expected_output, seqlens = process_data(songs, n_steps)
+    lstm.trainLSTM(expected_output, 20, report_interval=report_interval, seqlens=seqlens)
     lstm.trainAdversarially(expected_output, epochs, report_interval=report_interval, seqlens=seqlens)
     n_steps += 10
 
